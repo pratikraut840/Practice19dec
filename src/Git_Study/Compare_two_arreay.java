875ecd10410fb85c4a7942d5b616e5e51af1ed96
Hp <Hp@LAPTOP-FVMIP8G1>
2021-12-19 15:16:08 +0530

    I am send program

---
+++ src/Git_Study/Compare_two_arreay.java
@@ -0,0 +1,23 @@
+package Git_Study;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Compare_two_arreay {
+
+	public static void main(String[] args) 
+	{
+		int ar1[]= {1,2,3};
+		int ar2[]= {4,5,6};
+		int ar3[]= {1,2,3};
+		System.out.println(Arrays.equals(ar1, ar2));
+		System.out.println(Arrays.equals(ar1, ar3));
+		
+		
+		
+
+
+	}
+
+}
