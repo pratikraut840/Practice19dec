ce57b0a6830d775230d318783a7cb58e58bf79fd
HP <HP@LAPTOP-BK550KLF>
2021-12-19 15:07:42 +0530

    factorial prob by anant

---
+++ src/factorial.java
@@ -0,0 +1,18 @@
+
+
+public class factorial {
+
+	public static void main(String[] args)
+	{
+		
+		int a= 3;
+		int b=1;
+		
+		for (int i=a;i>=1;i--)
+		{
+			b=b*a;
+		}
+System.out.println(b);
+	}
+
+}
