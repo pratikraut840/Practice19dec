d268df89bbdcce9914868aff2071f80990fdd1d3
pratikraut840 <96044032+pratikraut840@users.noreply.github.com>
2021-12-19 13:56:17 +0530

    Add files via upload
    Add for TE1,TE2,TE3 and import this in your local machine

---
+++ Split_use.java
@@ -0,0 +1,18 @@
+package String_Study;
+
+public class Split_use {
+
+	public static void main(String[] args) {
+		
+		String given=" I : LOVE : MY : INDIA";
+		String ref=":";
+		
+		String out[]=given.split(ref);
+		
+		for(int i=0;i<=3;i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
