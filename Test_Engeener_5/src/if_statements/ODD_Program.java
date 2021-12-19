f1c72112742c4efb5b2ccd52642cb2a5fcf8252a
sony <sony@DESKTOP-4LKEELR>
2021-12-19 14:45:52 +0530

    New round

---
+++ Test_Engeener_5/src/if_statements/ODD_Program.java
@@ -0,0 +1,23 @@
+package if_statements;
+
+
+public class ODD_Program {
+
+	public static void main(String[] args) 
+	{
+		
+		for(int i=1;i<=10;i++) 
+		{
+			System.out.println(i);
+			i=i;
+		}
+
+		
+		}
+			
+		}
+		
+		
+	
+
+
